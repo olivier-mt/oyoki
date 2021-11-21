b0a7d3a0bbf0213cbc91288297c6a77a054f6e31
Olivier Mountou <oliviermountou@MacBook-Pro-de-Olivier.local>
2021-11-21 14:13:25 +0100

    android notifications done

--- index.js
+++ index.js
@@ -5,6 +5,23 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {NavigationContainer} from '@react-navigation/native';
+import messaging from '@react-native-firebase/messaging';
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
+requestUserPermission();
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
